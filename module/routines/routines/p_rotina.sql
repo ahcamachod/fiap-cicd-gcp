ae403dfdf7ce2486daf165d657ff00fb750cdb01
Alvaro Camacho <60886111+ahcamachod@users.noreply.github.com>
2025-03-15 21:45:53 -0300

    Update p_rotina.sql

---
+++ module/routines/routines/p_rotina.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE PROCEDURE `{PROJECT_ID}.fiap_cicd_{ENV}_agnv.p_rotina`()
+BEGIN 
+
+BEGIN TRANSACTION;
+
+insert into   `{PROJECT_ID}.fiap_cicd_{ENV}_agnv.tbl_data`
+SELECT * FROM `{PROJECT_ID}.fiap_cicd_{ENV}_agnv.view_data`
+
+COMMIT TRANSACTION;
+
+END;
